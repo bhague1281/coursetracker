ee3d7d753dae423d27fda313f1542620eb8be580
bhague1281 <bhague1281@gmail.com>
2016-01-12 14:26:53 -0800

    Disabled attendance directive

--- public/app/directives.js
+++ public/app/directives.js
@@ -54,31 +54,31 @@ angular.module('CourseTrackerDirectives', ['chart.js', 'CourseServices', 'ui.boo
 
   $scope.student = Students.studentObject(student.githubUsername);
 }])
-.directive('attendanceGraph', function() {
-  return {
-    restrict: 'E',
-    templateUrl: 'app/views/directives/attendanceGraph.html',
-    scope: {
-      cohort: '='
-    },
-    controller: ['$scope', 'Attendance', '$firebaseArray', function($scope, Attendance, $firebaseArray) {
-      // Attendance.ref.child('WDI 04').child()
-      var attendanceRecords = $firebaseArray(Attendance.ref.child($scope.cohort));
-      $scope.data = [[]];
-      $scope.labels = [];
-      $scope.series = ['Attendance'];
-      var updateAttendanceRecords = function() {
-        $scope.data[0] = [];
-        $scope.labels = [];
-        attendanceRecords.forEach(function(item) {
-          $scope.data[0].push(item.count);
-          $scope.labels.push(item.$id);
-        });
-      };
-      attendanceRecords.$loaded().then(function() {
-        updateAttendanceRecords();
-        attendanceRecords.$watch(updateAttendanceRecords);
-      });
-    }]
-  }
-})
+// .directive('attendanceGraph', function() {
+//   return {
+//     restrict: 'E',
+//     templateUrl: 'app/views/directives/attendanceGraph.html',
+//     scope: {
+//       cohort: '='
+//     },
+//     controller: ['$scope', 'Attendance', '$firebaseArray', function($scope, Attendance, $firebaseArray) {
+//       // Attendance.ref.child('WDI 04').child()
+//       var attendanceRecords = $firebaseArray(Attendance.ref.child($scope.cohort));
+//       $scope.data = [[]];
+//       $scope.labels = [];
+//       $scope.series = ['Attendance'];
+//       var updateAttendanceRecords = function() {
+//         $scope.data[0] = [];
+//         $scope.labels = [];
+//         attendanceRecords.forEach(function(item) {
+//           $scope.data[0].push(item.count);
+//           $scope.labels.push(item.$id);
+//         });
+//       };
+//       attendanceRecords.$loaded().then(function() {
+//         updateAttendanceRecords();
+//         attendanceRecords.$watch(updateAttendanceRecords);
+//       });
+//     }]
+//   }
+// })
